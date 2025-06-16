ed9942294f92ae73a486e311af13c7f4ae49e43e
TheYamo <87617257+EpicYamo@users.noreply.github.com>
2025-06-16 15:16:12 +0300

    new redirection algorithm
    new redirection algorithm

--- parser_pt_two.c
+++ parser_pt_two.c
@@ -12,6 +12,12 @@
 
 #include "minishell.h"
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+static int	validate_redirection_syntax(char **tokens, size_t *i);
+static int	validate_redirection_file(const char *filename, int type);
+static int	assign_redirection(t_command *cmd, char **tokens, size_t *i, int type);
 
 t_command	*new_command(t_gc *gc)
 {
@@ -34,22 +40,79 @@ t_command	*new_command(t_gc *gc)
 
 int	handle_redirection_token(char **tokens, size_t *i, t_command *cmd)
 {
-	if ((tokens[*i][0] == '<' || tokens[*i][0] == '>') &&
-		(tokens[*i + 1] == NULL || tokens[*i + 1][0] == '<' ||
-		tokens[*i + 1][0] == '>' || tokens[*i + 1][0] == '|'))
+	if (validate_redirection_syntax(tokens, i) == -1)
+		return (-1);
+	if (ft_strcmp(tokens[*i], "<") == 0)
+		return (assign_redirection(cmd, tokens, i, 0));
+	else if (ft_strcmp(tokens[*i], ">") == 0)
+		return (assign_redirection(cmd, tokens, i, 1));
+	else if (ft_strcmp(tokens[*i], ">>") == 0)
+		return (assign_redirection(cmd, tokens, i, 2));
+	else if (ft_strcmp(tokens[*i], "<<") == 0)
 	{
-		printf("\033[0;31mSyntax error: near unexpected token `%s'\033[0m\n", tokens[*i]);
+		cmd->infile = tokens[++(*i)];
+		cmd->heredoc = 1;
+		return (1);
+	}
+	return (0);
+}
+
+static int	validate_redirection_syntax(char **tokens, size_t *i)
+{
+	if ((ft_strcmp(tokens[*i], "<") == 0 || ft_strcmp(tokens[*i], ">") == 0 ||
+		 ft_strcmp(tokens[*i], "<<") == 0 || ft_strcmp(tokens[*i], ">>") == 0))
+	{
+		if (tokens[*i + 1] == NULL)
+		{
+			printf("bash: syntax error near unexpected token `newline'\n");
+			return (-1);
+		}
+		else if (ft_strcmp(tokens[*i + 1], "<") == 0 || ft_strcmp(tokens[*i + 1], ">") == 0 ||
+				 ft_strcmp(tokens[*i + 1], "<<") == 0 || ft_strcmp(tokens[*i + 1], ">>") == 0 ||
+				 ft_strcmp(tokens[*i + 1], "|") == 0)
+		{
+			printf("bash: syntax error near unexpected token `%s'\n", tokens[*i + 1]);
+			return (-1);
+		}
+	}
+	return (0);
+}
+
+static int	assign_redirection(t_command *cmd, char **tokens, size_t *i, int type)
+{
+	if (validate_redirection_file(tokens[*i + 1], type) == -1)
+		return (-1);
+	if (type == 0)
+	{
+		cmd->infile = tokens[++(*i)];
+		cmd->heredoc = 0;
+	}
+	else
+	{
+		cmd->outfile = tokens[++(*i)];
+		cmd->append = (type == 2);
+	}
+	return (1);
+}
+
+static int	validate_redirection_file(const char *filename, int type)
+{
+	int	fd;
+
+	if (type == 1)
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (type == 2)
+		fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (type == 0)
+		fd = open(filename, O_RDONLY);
+	else
+		return (-1);
+
+	if (fd == -1)
+	{
+		perror(filename);
 		return (-1);
 	}
-	if (tokens[*i][0] == '<' && tokens[*i][1] == '\0')
-		cmd->infile = tokens[++(*i)], cmd->heredoc = 0;
-	else if (tokens[*i][0] == '>' && tokens[*i][1] == '\0')
-		cmd->outfile = tokens[++(*i)], cmd->append = 0;
-	else if (tokens[*i][0] == '<' && tokens[*i][1] == '<' && tokens[*i][2] == '\0')
-		cmd->infile = tokens[++(*i)], cmd->heredoc = 1;
-	else if (tokens[*i][0] == '>' && tokens[*i][1] == '>' && tokens[*i][2] == '\0')
-		cmd->outfile = tokens[++(*i)], cmd->append = 1;
-	else
-		return (0);
-	return (1);
+	close(fd);
+	return (0);
 }
