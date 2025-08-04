edab71541958d08324cc1e7dfbe5895be91540ee
TheYamo <87617257+EpicYamo@users.noreply.github.com>
2025-08-04 15:50:00 +0300

    fix
    cat << EOF1 | cat << EOF2
    error fix

--- executor/executor_pt_one.c
+++ executor/executor_pt_one.c
@@ -6,7 +6,7 @@
 /*   By: aaycan <aaycan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 03:26:26 by aaycan            #+#    #+#             */
-/*   Updated: 2025/08/01 01:09:14 by aaycan           ###   ########.fr       */
+/*   Updated: 2025/08/04 15:48:13 by aaycan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void	init_io(t_io *io, t_command *cmd);
+static void	init_io(t_io *io, t_command *cmd, char **unlink_file);
 static int	skip_command(t_command **cmd);
 static int	apply_pipe(t_io *io);
-static void	return_to_original_state(t_io io);
+static void	return_to_original_state(t_io io, char *unlink_file);
 
 void	command_executor(t_command *cmd, t_gc *gc, char **formatted_line,
 	t_env *env_list)
 {
 	t_io	io;
+	char	*unlink_file;
 
-	init_io(&io, cmd);
+	unlink_file = NULL;
+	init_io(&io, cmd, &unlink_file);
 	if (execute_single_built_in_command(&cmd, gc,
 			formatted_line, env_list) != 0)
 		return ;
@@ -44,14 +46,12 @@ void	command_executor(t_command *cmd, t_gc *gc, char **formatted_line,
 			exec_built_in_com_in_child_proc(cmd, gc, formatted_line, env_list);
 		else
 			execute_non_built_in_command(cmd, env_list);
-		if (cmd->heredoc == 1)
-			unlink(cmd->infile);
 		cmd = cmd->next;
 	}
-	return_to_original_state(io);
+	return_to_original_state(io, unlink_file);
 }
 
-static void	init_io(t_io *io, t_command *cmd)
+static void	init_io(t_io *io, t_command *cmd, char **unlink_file)
 {
 	(*io).prev_fd = -1;
 	(*io).original_stdin = dup(STDIN_FILENO);
@@ -59,6 +59,12 @@ static void	init_io(t_io *io, t_command *cmd)
 	(*io).exit_status = 0;
 	if (cmd)
 		cmd->io = io;
+	while (cmd)
+	{
+		if (cmd->heredoc == 1)
+			(*unlink_file) = cmd->infile;
+		cmd = cmd->next;
+	}
 }
 
 static int	skip_command(t_command **cmd)
@@ -81,7 +87,7 @@ static int	apply_pipe(t_io *io)
 	return (0);
 }
 
-static void	return_to_original_state(t_io io)
+static void	return_to_original_state(t_io io, char *unlink_file)
 {
 	dup2(io.original_stdin, STDIN_FILENO);
 	dup2(io.original_stdout, STDOUT_FILENO);
@@ -90,4 +96,6 @@ static void	return_to_original_state(t_io io)
 	if (io.prev_fd != -1)
 		close(io.prev_fd);
 	signal(SIGINT, handle_sigint);
+	if (unlink_file != NULL)
+		unlink(unlink_file);
 }
