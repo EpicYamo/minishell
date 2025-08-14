7615b6c006fde9b881fee73cd7930f4cb3e8a389
arda <m.ardaaycan@gmail.com>
2025-08-14 20:07:36 +0300

    some fixes
    changed printf with write function for printing the banner because the buffer of printf might cause trouble in some rare cases

--- executor/executor_pt_two.c
+++ executor/executor_pt_two.c
@@ -6,7 +6,7 @@
 /*   By: aaycan <aaycan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:04:26 by aaycan            #+#    #+#             */
-/*   Updated: 2025/08/10 21:15:00 by aaycan           ###   ########.fr       */
+/*   Updated: 2025/08/14 19:39:05 by aaycan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <readline/readline.h>
 
 static void	io_prep(t_command *cmd);
 static void	close_fds_for_child_proc(t_command *cmd);
@@ -24,6 +25,7 @@ void	exec_built_in_com_in_child_proc(t_command *cmd, t_gc *gc,
 	char **formatted_line, t_env **env_list)
 {
 	pid_t	proc_pid;
+	int		exit_val;
 
 	io_prep(cmd);
 	proc_pid = fork();
@@ -35,16 +37,18 @@ void	exec_built_in_com_in_child_proc(t_command *cmd, t_gc *gc,
 			dup2(cmd->io->pipe_fd[1], STDOUT_FILENO);
 		close_fds_for_child_proc(cmd);
 		execute_built_in_commands(cmd, gc, formatted_line, env_list);
-		exit((*cmd->io->exit_stat_ptr));
+		exit_val = (*cmd->io->exit_stat_ptr);
+		rl_clear_history();
+		gc_collect_all(gc);
+		free_string_array(formatted_line);
+		free_env_list((*env_list));
+		exit(exit_val);
 	}
-	if (proc_pid < 0)
-	{
-		perror("fork");
-		return ;
-	}
-	cmd->io->pids[cmd->io->proc_count] = proc_pid;
-	cmd->io->proc_count += 1;
+	if (proc_pid > 0)
+		cmd->io->pids[(cmd->io->proc_count)++] = proc_pid;
 	close(cmd->io->pipe_fd[1]);
+	if (proc_pid < 0)
+		perror("fork");
 }
 
 static void	io_prep(t_command *cmd)
